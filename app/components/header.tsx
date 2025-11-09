3b771b6efe56a7c095bb0a4314f13df0ad096583
jyhgyh <jyhgyh@WIN-GF1QG24R6O1.localdomain>
2025-11-09 23:21:41 +0100

    feat: cv add

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -72,7 +72,7 @@ export default function Header() {
             ))}
             <li>
               <a
-                href="./CV_Rinat_Hirfanov.pdf"
+                href="/CV_Rinat_Hirfanov.pdf"
                 download="Rinat_Hirfanov_CV.pdf"
                 className="ml-2 text-text-red hover:text-blue-600 transition-colors"
               >
